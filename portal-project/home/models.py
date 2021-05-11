cae11ec46ba74adeae05c1bbfeba8bd625eeb65c
ShilkinYuV <yurii.shilkin@gmail.com>
2021-05-11 10:47:57 +0300

    magrin bottom 200px detais tex

--- portal-project/home/models.py
+++ portal-project/home/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 class News(models.Model):
-    text_field = models.CharField(max_length=250)
+    text_field = models.TextField()
     image_field = models.ImageField()
+
+    def get_title(self):
+    	self.title = self.text_field[:50]
+    	return self.title
+    	
+    		
