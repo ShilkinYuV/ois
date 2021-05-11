c42adb1564b03c126281db4b6b026a2b1cd970db
ShilkinYuV <yurii.shilkin@gmail.com>
2021-05-11 12:58:40 +0300

    update realisation views news and add border in table

--- portal-project/home/views.py
+++ portal-project/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import News
 
 def home(request):
-	news = News.objects.all()
+	news = News.objects.all().order_by('-id')[:4]
 	return render(request, 'home/index.html', {'news': news})
 
 def details(request, id):
