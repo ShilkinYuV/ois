2607aafd5d4b83b216a3adedbc94b36deb30e952
ShilkinYuV <yurii.shilkin@gmail.com>
2021-05-11 08:15:32 +0300

    Update settings.py

--- portal-project/portal/settings.py
+++ portal-project/portal/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '28Ofebov',
+        'PASSWORD': 'P@ssw0rd',
         'HOST': 'localhost',
         'PORT': '5432',
     }
